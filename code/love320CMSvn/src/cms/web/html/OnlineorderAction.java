3b941fa4876de8188a8959215e0349eb027f3fd6
admin@love320.com <admin@love320.com@55631e73-0142-5f3d-69cc-c878d74f2436>
2012-04-08 12:39:11 +0000

    加入版权

--- code/love320CMSvn/src/cms/web/html/OnlineorderAction.java
+++ code/love320CMSvn/src/cms/web/html/OnlineorderAction.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2010-2011 love320.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * 
+ * Founder admin@love320.com
+ * 
+ * http://www.love320.com
+ */
 package cms.web.html;
 
 import org.apache.struts2.convention.annotation.Result;
