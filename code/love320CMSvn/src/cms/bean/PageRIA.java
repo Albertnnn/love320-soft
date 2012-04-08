3372bd535d52fda9e82bd64cdeebf8c7e33db648
admin <admin@love320.com>
2012-04-08 12:39:11 +0000

    加入版权

--- code/love320CMSvn/src/cms/bean/PageRIA.java
+++ code/love320CMSvn/src/cms/bean/PageRIA.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2010-2011 love320.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * 
+ * Founder admin@love320.com
+ * 
+ * http://www.love320.com
+ */
 package cms.bean;
 
 import org.springside.modules.orm.Page;
