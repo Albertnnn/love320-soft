ae93b299aa606797e0c618996fc9864933c06768
admin@love320.com <admin@love320.com@55631e73-0142-5f3d-69cc-c878d74f2436>
2012-05-06 10:51:53 +0000

    添加新类，功能:从xml数据格式转换为label对象

--- code/templateParser/src/com/love320/templateparser/templateprocess/impl/TemplateProcessImpl.java
+++ code/templateParser/src/com/love320/templateparser/templateprocess/impl/TemplateProcessImpl.java
@@ -1,8 +1,11 @@
 package com.love320.templateparser.templateprocess.impl;
 
 import java.io.File;
+import java.util.List;
 
+import com.love320.templateparser.factory.entity.Label;
 import com.love320.templateparser.io.FileToString;
+import com.love320.templateparser.label.XMLToLabel;
 import com.love320.templateparser.templateprocess.Separation;
 import com.love320.templateparser.templateprocess.TemplateProcess;
 
@@ -14,6 +17,7 @@ public class TemplateProcessImpl implements TemplateProcess {
 	private FileToString fileToString ;//文件获取对象
 	private String templateDir = ""; //模板目录
 	private Separation separation;//分离器(内容与标签分离)
+	private XMLToLabel xmlToLabel;//转换器，从xml数据格式转换为label对象
 
 	public void setTemplateDir(String templateDir) {
 		this.templateDir = templateDir;
@@ -26,6 +30,10 @@ public class TemplateProcessImpl implements TemplateProcess {
 	public void setSeparation(Separation separation) {
 		this.separation = separation;
 	}
+	
+	public void setXmlToLabel(XMLToLabel xmlToLabel) {
+		this.xmlToLabel = xmlToLabel;
+	}
 
 	@Override
 	public String get(String path) {
@@ -34,8 +42,11 @@ public class TemplateProcessImpl implements TemplateProcess {
 
 	@Override
 	public String get(File file) {
-		String templateFileStr = fileToString.get(file);
-		System.out.println(">>>"+separation.getXML(templateFileStr));
+		String templateFileStr = fileToString.get(file);//从文件中读取内容到字符串中
+		String tempXML = separation.getXML(templateFileStr);//分离内容与标签并以xml管理，返回xml字符串
+		List<Label> labelList = xmlToLabel.get(tempXML);//从xml数据格式转换为label对象
+		
+		System.out.println(labelList.size());
 		
 		return null;
 	}
